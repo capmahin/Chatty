b4d6316fff246093b331ca31ebf9c02488bf72d4
capmahin <mahinulhuque1996@gmail.com>
2025-04-05 12:22:34 +0600

    solve should be

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.use(express.json());
-
 app.use(cookieParser());
 app.use(
   cors({
@@ -25,7 +28,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
